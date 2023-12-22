3b6450ba8f16d56f26f8ea22e0a7f4e6046b370b
Beksultan Dzhienbekov <bekadj14@gmail.com>
2023-12-22 18:51:10 +0600

    перенес роутер в main tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import './App.css'
 import SearchPage from "./components/SearchPage";
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import {BrowserRouter as Routes, Route} from "react-router-dom";
 import ShowDetails from "./components/ShowDetailsPage.tsx";
 
 function App() {
 
   return (
-    <Router>
       <div>
-          <Routes>
-              <Route path="/" element={<SearchPage/>}/>
-              <Route path="/shows/:id" element={<ShowDetails/>}/>
-          </Routes>
+          <div>
+              <Routes>
+                  <Route path="/" element={<SearchPage/>}/>
+                  <Route path="/shows/:id" element={<ShowDetails/>}/>
+              </Routes>
+          </div>
       </div>
-    </Router>
   )
 }
 
