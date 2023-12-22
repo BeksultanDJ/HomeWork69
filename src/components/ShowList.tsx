74c6020bae5431aa590857df19af6b9a40f55311
Beksultan Dzhienbekov <bekadj14@gmail.com>
2023-12-22 16:20:16 +0600

    Добавил условие если инпут пуст ничего не выводить

--- src/components/ShowList.tsx
+++ src/components/ShowList.tsx
@@ -2,16 +2,18 @@ import React from 'react';
 
 interface ShowListProps {
     suggestions: string[];
-    handleSuggestionClick: (suggestion: string) => void;
 }
 
-const ShowList: React.FC<ShowListProps> = ({ suggestions}) => {
+const ShowList: React.FC<ShowListProps> = ({ suggestions }) => {
+
+    if (!suggestions || suggestions.length === 0) {
+        return null;
+    }
+
     return (
         <div className="searchList">
             {suggestions.map((item, index) => (
-                <p key={index} >
-                    {item}
-                </p>
+                <p key={index}>{item}</p>
             ))}
         </div>
     );
