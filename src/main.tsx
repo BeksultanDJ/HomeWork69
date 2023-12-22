09b9437c180c41057b32a28660de320bce4e6a1e
Beksultan Dzhienbekov <bekadj14@gmail.com>
2023-12-22 18:50:56 +0600

    настроил роутер

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { Provider } from 'react-redux';
 import store from './components/store/store';
 import App from './App';
 import './index.css';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 const root = createRoot(document.getElementById('root'));
 
 root.render(
-    <Provider store={store}>
-        <App />
-    </Provider>
+    <Router>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </Router>
 );
