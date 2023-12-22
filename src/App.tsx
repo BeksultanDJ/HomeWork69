8a75920842ea79535f878be78395325bb5154837
Beksultan Dzhienbekov <bekadj14@gmail.com>
2023-12-22 16:48:54 +0600

    подключил Routes и Route

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import './App.css'
 import SearchPage from "./components/SearchPage";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import ShowDetails from "./components/ShowDetailsPage.tsx";
 
 function App() {
 
   return (
-    <>
+    <Router>
       <div>
-          <SearchPage />
+          <Routes>
+              <Route path="/" element={<SearchPage/>}/>
+              <Route path="/shows/:id" element={<ShowDetails/>}/>
+          </Routes>
       </div>
-    </>
+    </Router>
   )
 }
 
