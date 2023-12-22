26de28d0cc87bd1c3257d103b71684d6ef8b9a54
Beksultan Dzhienbekov <bekadj14@gmail.com>
2023-12-22 18:54:32 +0600

    вернул как было

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
 import './App.css'
 import SearchPage from "./components/SearchPage";
-import {BrowserRouter as Routes, Route} from "react-router-dom";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import ShowDetails from "./components/ShowDetailsPage.tsx";
 
 function App() {
 
   return (
       <div>
-          <div>
-              <Routes>
-                  <Route path="/" element={<SearchPage/>}/>
-                  <Route path="/shows/:id" element={<ShowDetails/>}/>
-              </Routes>
-          </div>
+    <Router>
+      <div>
+          <Routes>
+              <Route path="/" element={<SearchPage/>}/>
+              <Route path="/shows/:id" element={<ShowDetails/>}/>
+          </Routes>
+      </div>
+    </Router>
       </div>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,11 @@ import { Provider } from 'react-redux';
 import store from './components/store/store';
 import App from './App';
 import './index.css';
-import { BrowserRouter as Router } from 'react-router-dom';
 
 const root = createRoot(document.getElementById('root'));
 
 root.render(
-    <Router>
-        <Provider store={store}>
-            <App />
-        </Provider>
-    </Router>
+    <Provider store={store}>
+        <App />
+    </Provider>
 );
