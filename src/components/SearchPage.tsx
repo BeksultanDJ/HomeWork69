2e0c0393e9d02815bfd7f6147a8e2d2e64d05a8d
Beksultan Dzhienbekov <bekadj14@gmail.com>
2023-12-22 16:06:16 +0600

    все действия с апи перенес в компонент со слайсами импортировал слайсы вызываю фуекции из showListSlice c помощью dispatch

--- src/components/SearchPage.tsx
+++ src/components/SearchPage.tsx
@@ -1,37 +1,34 @@
-import axios from 'axios';
 import React, { useState } from 'react';
-import ShowList from "./ShowList";
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchSuggestions } from './showListSlice.ts';
+import ShowList from './ShowList';
 
 const SearchPage = () => {
-    const [suggestions, setSuggestions] = useState<string[]>([]);
     const [searchTerm, setSearchTerm] = useState('');
+    const dispatch = useDispatch();
+    const suggestions = useSelector((state: any) => state.shows.suggestions);
 
-    const handleInputChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const value = event.target.value;
         setSearchTerm(value);
 
-        try {
-            const response = await axios.get(`http://api.tvmaze.com/search/shows?q=${value}`);
-            const data = response.data.map((item: any) => item.show.name);
-
-            setSuggestions(data);
-        } catch (error) {
-            console.error('Error fetching data:', error);
+        if (value.trim() !== '') {
+            dispatch(fetchSuggestions(value));
         }
     };
 
     return (
         <div>
-            <form action="">
+            <form className="searchBar">
                 <input
+                    className="input"
                     type="text"
                     value={searchTerm}
                     onChange={handleInputChange}
                 />
             </form>
-            <button type="submit">Search</button>
 
-            <ShowList suggestions={suggestions}  />
+            <ShowList suggestions={suggestions} />
         </div>
     );
 };
