91d5d8853012bfd4a6cf509a192ec0e2f93ffeaa
Beksultan Dzhienbekov <bekadj14@gmail.com>
2023-12-22 15:04:43 +0600

    подключил провайдер подклбчил стор

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import store from './components/store/store';
+import App from './App';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const root = createRoot(document.getElementById('root'));
+
+root.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+);
